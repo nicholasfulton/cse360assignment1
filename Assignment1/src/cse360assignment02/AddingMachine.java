4847b2605984b3267be4c6951337cdafbda852d3
Nicholas Fulton <nicholasfulton@outlook.com>
2020-10-04 12:32:56 -0700

    Initial create

---
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,50 @@
+package cse360assignment02;
+
+/**
+ * Utility class which aids in the addition of numbers
+ */
+public class AddingMachine {
+	private int total;
+
+	/**
+	 * Default constructor, initializes members
+	 */
+	public AddingMachine() {
+		total = 0; // not needed - included for clarity
+	}
+
+	/**
+	 * Get the total of previous addition operations
+	 * @return Sum of operands
+	 */
+	public int getTotal() {
+		return 0;
+	}
+
+	/**
+	 * Add value to total
+	 * @param value Amount by which to increase total
+	 */
+	public void add(int value) {
+	}
+
+	/**
+	 * Subtract value from total
+	 * @param value Amount by which to decrease total
+	 */
+	public void subtract(int value) {
+	}
+
+	/**
+	 * Represent this object as a string
+	 */
+	public String toString() {
+		return "";
+	}
+
+	/**
+	 * Reset object to default values
+	 */
+	public void clear() {
+	}
+}
